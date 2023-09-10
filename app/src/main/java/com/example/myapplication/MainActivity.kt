02367449cc6324e811cfc98ab132dc5dac444dd2
nrkay <nkhairiyah018@gmail.com>
2023-09-10 13:44:43 +0700

    apk pemesanan

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -14,14 +15,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val groupButton = findViewById<RadioGroup>(R.id.radioGroupMenu)
         val result = findViewById<TextView>(R.id.result)
+        val menuCoffee = findViewById<CheckBox>(R.id.minumCoffee)
+        val menuJus = findViewById<CheckBox>(R.id.minumJus)
+        val menuMineral = findViewById<CheckBox>(R.id.minumMineral)
+        val btnChackOut = findViewById<Button>(R.id.btnCheckOut)
 
-//        method untuk groupButton setOnCheckedListener
-        groupButton.setOnCheckedChangeListener{ group, checkedId ->
-            val radioButton = findViewById<RadioButton>(checkedId)
-            val textSelect = radioButton.text.toString()
-            result.setText("Pilihan anda adalah $textSelect")
 
+//        Mendapatkan string dari checkbox yang akan dipilih
+        val textCoffee = menuCoffee.isChecked
+        val textJuss = menuJus.isChecked
+        val textMineral = menuMineral.isChecked
+
+// Membuat variabel untuk menyimpan teks dari RadioButton yang dipilih
+        var resultString = ""
+
+//        menjalankan radioGroup ketika di klk oleh user
+            groupButton.setOnCheckedChangeListener{ group, checkedId ->
+                val radioButton = findViewById<RadioButton>(checkedId)
+                resultString = radioButton.text.toString()
+
+            }
+
+        // Mendengarkan perubahan CheckBox dan mengumpulkan teks yang dipilih
+        val selectedCheckBoxesText = StringBuilder()
+        menuCoffee.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                selectedCheckBoxesText.append("Coffee\n")
+            }
         }
+
+        menuJus.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                selectedCheckBoxesText.append("Jus\n")
+            }
+        }
+
+        menuMineral.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                selectedCheckBoxesText.append("Mineral\n")
+            }
+        }
+
+
+        btnChackOut.setOnClickListener{
+//            variabel selectedCheckBoxesText berupa object, untuk menampilkannya digunakan toString() untuk diubah ke string
+            val resultPesanan = "Pesanan Anda:\n $resultString\n${selectedCheckBoxesText.toString()}"
+            result.text = resultPesanan
+        }
+
+
 
 
 
