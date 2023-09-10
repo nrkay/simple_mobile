84f61ae7eec2d3f30251ddc4691d4408a0f8c94d
nrkay <nkhairiyah018@gmail.com>
2023-09-10 17:59:30 +0700

    add toast

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,64 +8,30 @@ import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val groupButton = findViewById<RadioGroup>(R.id.radioGroupMenu)
-        val result = findViewById<TextView>(R.id.result)
-        val menuCoffee = findViewById<CheckBox>(R.id.minumCoffee)
-        val menuJus = findViewById<CheckBox>(R.id.minumJus)
-        val menuMineral = findViewById<CheckBox>(R.id.minumMineral)
-        val btnChackOut = findViewById<Button>(R.id.btnCheckOut)
+        val textToast = findViewById<Button>(R.id.btnToast)
+        val templateToasr = findViewById<Button>(R.id.templateToast)
+//        val csToast = findViewById<ConstraintLayout>(R.id.toastTemplate)
 
-
-//        Mendapatkan string dari checkbox yang akan dipilih
-        val textCoffee = menuCoffee.isChecked
-        val textJuss = menuJus.isChecked
-        val textMineral = menuMineral.isChecked
-
-// Membuat variabel untuk menyimpan teks dari RadioButton yang dipilih
-        var resultString = ""
-
-//        menjalankan radioGroup ketika di klk oleh user
-            groupButton.setOnCheckedChangeListener{ group, checkedId ->
-                val radioButton = findViewById<RadioButton>(checkedId)
-                resultString = radioButton.text.toString()
-
-            }
-
-        // Mendengarkan perubahan CheckBox dan mengumpulkan teks yang dipilih
-        val selectedCheckBoxesText = StringBuilder()
-        menuCoffee.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                selectedCheckBoxesText.append("Coffee\n")
-            }
+//        membuat toast biasa
+        textToast.setOnClickListener{
+            Toast.makeText(this, "Ini adalah toast", Toast.LENGTH_SHORT).show()
         }
 
-        menuJus.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                selectedCheckBoxesText.append("Jus\n")
+//        membuat template toast
+        templateToasr.setOnClickListener{
+            Toast(this).apply {
+                duration = Toast.LENGTH_SHORT
+                view = layoutInflater.inflate(R.layout.template_toast,  findViewById<ConstraintLayout>(R.id.toastTemplate))
+                show()
             }
         }
-
-        menuMineral.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                selectedCheckBoxesText.append("Mineral\n")
-            }
-        }
-
-
-        btnChackOut.setOnClickListener{
-//            variabel selectedCheckBoxesText berupa object, untuk menampilkannya digunakan toString() untuk diubah ke string
-            val resultPesanan = "Pesanan Anda:\n $resultString\n${selectedCheckBoxesText.toString()}"
-            result.text = resultPesanan
-        }
-
-
-
-
 
 
     }
