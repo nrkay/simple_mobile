ffa446572e9c7b30b62f3bbd70dd569f6c0cd3bd
nrkay <nkhairiyah018@gmail.com>
2023-09-10 11:37:56 +0700

    Memilih menu makanan menggunakan radioGroup

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,25 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val input1 = findViewById<EditText>(R.id.inputPertama)
-        val input2 = findViewById<EditText>(R.id.inputKedua)
-        val btn = findViewById<Button>(R.id.btn)
-        val hasil = findViewById<TextView>(R.id.hasil)
+        val groupButton = findViewById<RadioGroup>(R.id.radioGroupMenu)
+        val result = findViewById<TextView>(R.id.result)
 
+//        method untuk groupButton setOnCheckedListener
+        groupButton.setOnCheckedChangeListener{ group, checkedId ->
+            val radioButton = findViewById<RadioButton>(checkedId)
+            val textSelect = radioButton.text.toString()
+            result.setText("Pilihan anda adalah $textSelect")
 
-
-        btn.setOnClickListener{
-            val bil1 = input1.text.toString().toInt()
-            val bil2 = input2.text.toString().toInt()
-            val hasilPenjumlahan = bil1 + bil2
-            hasil.text = hasilPenjumlahan.toString()
         }
+
+
 
 
     }
